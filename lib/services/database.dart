e33323e1d95b230cf1ee05b68691834f1f3cce6a
PeymanBozorgnezhad <bozorgnezhadpeyman@gmail.com>
2022-04-21 01:29:13 +0430

    addrd new query functions

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,10 +1,50 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseMethods {
-  Future<QuerySnapshot<Map<String, dynamic>>>  getUserByUsername(String username) async {
+  Future<QuerySnapshot<Map<String, dynamic>>> getUserByUsername(
+      String username) async {
     return await FirebaseFirestore.instance
         .collection('users')
         .where("username", isEqualTo: username)
         .get();
+  }
+
+  createChatRoom({required String chatRoomId, required charRoomMap}) {
+    FirebaseFirestore.instance
+        .collection('ChatRoom')
+        .doc(chatRoomId)
+        .set(charRoomMap)
+        .catchError(
+          (e) => print(
+            e.toString(),
+          ),
+        );
+  }
+
+  Future<QuerySnapshot<Map<String, dynamic>>> getUserByEmail(
+      String email) async {
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .where("email", isEqualTo: email)
+        .get();
+  }
+
+  addConversationMessage({required String chatRoomId, required messageMap}) {
+    FirebaseFirestore.instance
+        .collection('ChatRoom')
+        .doc(chatRoomId)
+        .collection('chats')
+        .add(messageMap)
+        .catchError((e) {
+      print(e.toString());
+    });
+  }
+
+  getConversationMessage({required String chatRoomId}) {
+    return FirebaseFirestore.instance
+        .collection('ChatRoom')
+        .doc(chatRoomId)
+        .collection('chats')
+        .snapshots();
   }
 }
