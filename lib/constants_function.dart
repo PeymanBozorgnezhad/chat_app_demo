8939820c0610ee3a62b0470e06f3d85bc7c6afe4
PeymanBozorgnezhad <bozorgnezhadpeyman@gmail.com>
2022-04-22 13:56:09 +0430

    We passed the chatRoomId variable to the chatRoomScreen

--- lib/constants_function.dart
+++ lib/constants_function.dart
@@ -1,16 +1,27 @@
+import 'package:chat_app/screens/conversation_screen.dart';
 import 'package:chat_app/screens/home_screen.dart';
 import 'package:chat_app/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/chat_room_screen.dart';
 import 'screens/login_screen.dart';
 
-kNavigator(BuildContext context, String path) {
-  if (path == 'contact') {
+kNavigator(BuildContext context, String path, String chatRoomId) {
+  if (path == 'contact' || path == 'conversation') {
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (BuildContext ctx) {
-          return const SearchScreen();
+          if (path == 'contact') {
+            return const SearchScreen();
+          } //
+          else if (path == 'conversation') {
+            return ConversationScreen(
+              chatRoomId: chatRoomId,
+            );
+          } //
+          else {
+            return const ChatRoomScreen();
+          }
         },
       ),
     );
@@ -33,7 +44,7 @@ kNavigator(BuildContext context, String path) {
           },
         ),
       );
-    }//
+    } //
     else if (path == 'signup-chatroom') {
       Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
         builder: (BuildContext context) {
