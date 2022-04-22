9290476b12ed57360632c4d4ebe3457b0fc82a3a
PeymanBozorgnezhad <bozorgnezhadpeyman@gmail.com>
2022-04-22 14:11:45 +0430

    create ChatRoomScrren and ChatRoomsTile Widget

--- lib/screens/chat_room_screen.dart
+++ lib/screens/chat_room_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/constants_function.dart';
 import 'package:chat_app/helper/util_functions.dart';
+import 'package:chat_app/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../model/user_model.dart';
@@ -16,18 +18,59 @@ class ChatRoomScreen extends StatefulWidget {
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
   FirebaseAuth auth = FirebaseAuth.instance;
   User? user = FirebaseAuth.instance.currentUser;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   UserModel loggedInUser = UserModel();
+  DatabaseMethods databaseMethods = DatabaseMethods();
+  late CollectionReference chatRoomRef;
+
+  Widget chatRoomList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: chatRoomRef.where('users', arrayContains: kMyName).snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('Not Exist documents'),
+            );
+          }
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              Map data = snapshot.data!.docs[index].data() as Map;
+              return ChatRoomsTile(
+                userName: data['chatroomId']
+                    .toString()
+                    .replaceAll('_', "")
+                    .replaceAll(kMyName, ""),
+                chatRoomId: data['chatroomId'],
+              );
+            },
+          );
+        } //
+        else {
+          return const Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(
+                Colors.blueGrey,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     getUserInfo();
+    chatRoomRef = firestore.collection('ChatRoom');
     super.initState();
   }
 
   getUserInfo() async {
-    setState(() {
-    });
+    setState(() {});
     kMyName = await UtilFunctions.getUserNameSharedPref();
   }
 
@@ -41,11 +84,60 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
         ),
         centerTitle: true,
       ),
+      body: chatRoomList(),
       floatingActionButton: FloatingActionButton(
         heroTag: 'unique_key',
-        onPressed: () => kNavigator(context, 'contact','-1'),
+        onPressed: () => kNavigator(context, 'contact', '-1'),
         child: const Icon(
           Icons.search,
+        ),
+      ),
+    );
+  }
+}
+
+class ChatRoomsTile extends StatelessWidget {
+  final String userName;
+  final String chatRoomId;
+
+  ChatRoomsTile({required this.userName, required this.chatRoomId});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        kNavigator(context, 'conversation', chatRoomId);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.purple.shade200,
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Row(
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Text(
+                userName.substring(0, 1).toUpperCase(),
+                style: kTextStyle.copyWith(fontSize: 15),
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Text(
+              userName,
+              style: kTextStyle.copyWith(fontSize: 15),
+            ),
+          ],
         ),
       ),
     );
